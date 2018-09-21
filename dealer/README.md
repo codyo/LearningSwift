ae6d0cfb9c547eda2d568f25546cc90191c157b7
Cody Tolmasoff <codytolmasoff@gmail.com>
2018-09-21 10:09:55 -0700

    initial project files for chapter 3

---
+++ dealer/README.md
@@ -0,0 +1,3 @@
+# dealer
+
+A description of this package.
---
+++ dealer/Sources/dealer/main.swift
@@ -0,0 +1,15 @@
+import DeckOfPlayingCards
+
+var deck = Deck.standard52CardDeck()
+deck.shuffle()
+
+for _ in 0...4
+{
+    guard let card = deck.deal() else
+    {
+        print("No More Cards!")
+        break
+    }
+    print(card)
+}
+
